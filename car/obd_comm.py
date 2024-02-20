4e5b17e475bf275ee6fd7dc96fc92dd23c00e972
Mini Sun <minisun@Minis-MacBook-Air.local>
2024-02-20 13:18:34 -0500

    add LEDs

--- car/obd_comm.py
+++ car/obd_comm.py
@@ -1,6 +1,6 @@
 import time
 import obd
-
+import RPi.GPIO as GPIO #install the GPIO Python Library
 
 class brakeStatus:
     # create list to store speed value and diff
@@ -26,39 +26,57 @@ class brakeStatus:
             if speedDiff[i] >= 0:
                 return False
         return True
+   
+# setup LEDs
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(14, GPIO.OUT)  # acceleration LED
+GPIO.setup(15, GPIO.OUT)  # braking LED
+GPIO.setup(17, GPIO.OUT)  # connection OK LED
+GPIO.setup(18, GPIO.OUT)  # getting speed measurements LED
+GPIO.setup(27, GPIO.OUT)  # Check for programing is running 
 
+while True:
+    GPIO.output(26, GPIO.HIGH)
+    # wait time
+    time.sleep(0.1)
+    # create OBD connection and return list of valid USB
+    portNum = obd.scan_serial()
 
-# wait time
-time.sleep(0.1)
-# create OBD connection and return list of valid USB
-portNum = obd.scan_serial()
+    if portNum:
+        # connect to the first port in the list
+        connection = obd.OBD(portNum[0])
+        if connection.is_connected():
+            brake_status = brakeStatus()
+            GPIO.output(17, GPIO.HIGH)
+            
+            while True:
+                GPIO.output(18, GPIO.HIGH)  
+                # send command and recieve response
+                speedCmd = connection.query(obd.commands.SPEED)
+                if not speedCmd.is_null():
+                    speed = speedCmd.value.magnitude
+                    print(f"Driving speed: {speed} mile/h ")
+                    
+                    # update brake status with the current speed and speed diff
+                    brake_status.updateSpeed(speed)
+                    brake_status.calSpeedDiff()
 
-if portNum:
-    # connect to the first port in the list
-    connection = obd.OBD(portNum[0])
-    if connection.is_connected():
-        brake_status = brakeStatus()
-
-        while True:
-            # send command and recieve response
-            speedCmd = connection.query(obd.commands.SPEED)
-            if not speedCmd.is_null():
-                speed = speedCmd.value.magnitude
-                print(f"Driving speed: {speed} mile/h ")
-
-                # update brake status with the current speed and speed diff
-                brake_status.updateSpeed(speed)
-                brake_status.calSpeedDiff()
-
-                # check and print
-                check = brake_status.checkBrakeStatus()
-                if check:
-                    print("Braking Status: Slowing Down")
-                else:
-                    print("Braking Status: Not Slowing Down")
-            # add a delay between queries to avoid overwhelming the OBD system
-            time.sleep(0.5)
+                    # check and print
+                    check = brake_status.checkBrakeStatus()
+                    if check:
+                        print("Braking Status: Slowing Down")
+                        GPIO.output(15, GPIO.HIGH)  # turn on brake LED
+                        GPIO.output(14, GPIO.LOW)   # turn off acceleration LED
+                    else:
+                        print("Braking Status: Not Slowing Down")
+                        GPIO.output(14, GPIO.HIGH)  # turn on acceleration LED
+                        GPIO.output(15, GPIO.LOW)   #turn off brake LED
+                # add a delay between queries to avoid overwhelming the OBD system
+                time.sleep(0.5)
+        else:
+            print("failed to established OBD connection")
     else:
-        print("failed to established OBD connection")
-else:
-    print("no valid USB ports found")
+        print("no valid USB ports found")
+
+    GPIO.output(26, GPIO.LOW)
