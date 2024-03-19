e0c2e7b02c03d9fd6b51cb0fbb3cf43c889a14c3
derek614 <144835147+derek614@users.noreply.github.com>
2024-03-19 18:32:37 -0400

    Update obd_comm.py - class removed, bugs fixed

--- car/obd_comm.py
+++ car/obd_comm.py
@@ -6,46 +6,41 @@ import logging
 #     filemode: "w" is overwrite, "a" is append
 logging.basicConfig(level=logging.DEBUG, filename="obd_log.log", filemode="a", format="%(asctime)s : %(message)s")
 
-class brakeStatus:
-    # create list to store speed value and diff
-    def __init__(self):
-        logging.debug("inside brakeStatus constructor") 
-        self.speedData = [0] * 5
-        self.speedDiff = [0] * 4
+speedData = [0] * 5
+speedDiff = [0] * 4
 
-    # append and remove the current speed
-    def updateSpeed(self, currentSpeed):
-        logging.debug(f"inside updateSpeed(), speed = {currentSpeed}") 
-        self.speedData.append(currentSpeed)
-        self.speedData.pop(0)
+# append and remove the current speed
+def updateSpeed(currentSpeed):
+    logging.debug(f"inside updateSpeed(), speed = {currentSpeed}") 
+    speedData.pop(0)
+    speedData.append(currentSpeed)
 
-    # calculate speed diff between the latest and oldest entries
-    def calcSpeedDiff(self):
-        diff = self.speedData[4] - self.speedData[3]
-        logging.debug(f"inside calcSpeedDiff(), speed difference: {diff}") 
-        #update the speed diff list
-        self.speedDiff.append(diff)
-        self.speedDiff.pop(0)
+# calculate speed diff between the latest and oldest entries
+def calcSpeedDiff():
+    diff = speedData[4] - speedData[3]
+    logging.debug(f"inside calcSpeedDiff(), speed difference: {diff}") 
+    #update the speed diff list
+    speedDiff.pop(0)
+    speedDiff.append(diff)
+    
 
-    # isBraking speed diff are negative to indicate slowing down
-    def checkBrakeStatus(self):
-        logging.debug(f"inside checkBrakeStatus(), speedDiff = {self.speedDiff[4]}") 
-        #for i in self.speedDiff:
-         #   print(f"speedDiff[{i}] = {self.speedDiff[i]}")  | example of intended output: "speedDiff[2] = -20"
-          #  if self.speedDiff[i] >= 0:
-           #     return False
-        if self.speedDiff[3] >= 0: # only isBraking most recent measurement
-            return False
-        else:
-            return True
+# isBraking speed diff are negative to indicate slowing down
+def checkBrakeStatus():
+    logging.debug(f"inside checkBrakeStatus(), speedDiff = {self.speedDiff[3]}") 
+    #for i in speedDiff:
+     #   print(f"speedDiff[{i}] = {speedDiff[i]}")  | example of intended output: "speedDiff[2] = -20"
+      #  if speedDiff[i] >= 0:
+       #     return False
+    if speedDiff[3] >= 0: # only isBraking most recent measurement
+        return False
+    else:
+        return True
 
 ports = obd.scan_serial()
 connection = obd.OBD(ports[0])
 cmd = obd.commands.SPEED
 
 logging.debug(f"number of ports found: {len(ports)}")
-
-brake_status = brakeStatus() # create new brakeStatus object
 
 while True:    
     # send command and recieve response
@@ -54,11 +49,11 @@ while True:
     logging.debug(f"Driving speed: {speed} mile/h") 
                 
     # update brake status with the current speed and speed diff
-    brake_status.updateSpeed(speed)
-    brake_status.calcSpeedDiff()
+    updateSpeed(speed)
+    calcSpeedDiff()
 
     # isBraking and print
-    isBraking = brake_status.checkBrakeStatus()
+    isBraking = checkBrakeStatus()
     
     if isBraking:
         logging.debug("Braking Status: Slowing Down") 
