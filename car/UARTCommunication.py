7bd360fda0e8c7a9a193e6e54e4d7719a8e80072
derek614 <144835147+derek614@users.noreply.github.com>
2024-03-28 13:31:36 -0400

    Update UARTCommunication.py - send variable data

--- car/UARTCommunication.py
+++ car/UARTCommunication.py
@@ -5,21 +5,15 @@
 
 import serial
 import time
-#import subprocess
-#import os
 
 #configure the serial port
 ser = serial.Serial("/dev/ttyS0", 9600)
 
 while True:
-    #give permissions to write
-    #subprocess.run("sudo chmod 666 /dev/ttyS0", shell=True)
-    
     #send data to FPGA
-    #speed = 250
-    #braking = "B"
-    #sendData = f"{braking} {speed}\r\n"
-    sendData = "test\r\n"
+    speed = 250
+    braking = "B"
+    sendData = f"{braking} {speed}\r\n"
     ser.write(bytes(sendData,'utf-8'))
     print(f"Sent: {sendData}")
   
