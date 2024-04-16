b71a2675a827dcf7363caf70d66cff4669712956
derek614 <144835147+derek614@users.noreply.github.com>
2024-04-16 14:04:07 -0400

    Update UARTCommunication.py - zero fill on left

--- car/UARTCommunication.py
+++ car/UARTCommunication.py
@@ -16,7 +16,7 @@ while True:
     braking = "B"
     sendData = f"{braking} {speed:0,.3f}\r\n"
     ser.write(bytes(sendData,'utf-8'))
-    print(f"Sent: {sendData}")
+    print(f"Sent: {sendData.zfill(7)}")
   
     #add delay
     time.sleep(1)
