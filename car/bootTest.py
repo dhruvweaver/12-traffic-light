8858081690c5054f1557f0b1bb7e174461de282c
Dhruv <dhruv.weaver@gmail.com>
2024-04-27 21:51:24 -0400

    car: remove extra scripts

--- car/bootTest.py
+++
@@ -1,3 +0,0 @@
-import logging
-logging.basicConfig(level=logging.DEBUG, filename="/home/team12/bootTest.log", filemode="a", format="%(asctime)s : %(message)s")
-logging.debug("test") 
--- car/portTest.py
+++
@@ -1,5 +0,0 @@
-import obd
-
-ports = obd.scan_serial()
-print(f"number of ports found: {len(ports)}")
-print(ports)
